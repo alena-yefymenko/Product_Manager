0def02b208f456f6198592736cf6c2bb4d0d5a2d
Alyona <110098585+alena-yefymenko@users.noreply.github.com>
2023-01-29 17:48:48 +0200

    Update ProductRepository.java

--- src/main/java/com/example/manageproduct/ProductRepository.java
+++ src/main/java/com/example/manageproduct/ProductRepository.java
@@ -2,8 +2,5 @@ package com.example.manageproduct;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// Use for query's to db
-// also can extend CrudRepository
-
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }

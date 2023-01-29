c5b09767e5ef728234206962b07f355bfa11be98
Alyona <110098585+alena-yefymenko@users.noreply.github.com>
2023-01-29 17:50:25 +0200

    Update ProductService.java

--- src/main/java/com/example/manageproduct/ProductService.java
+++ src/main/java/com/example/manageproduct/ProductService.java
@@ -1,14 +1,14 @@
 package com.example.manageproduct;
 import org.springframework.stereotype.Service;
 import java.util.List;
-//Injection
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
-    } // Initialise ore @Autowiring
+    } 
 
     public Product getById(Long id) {
         return productRepository.getReferenceById(id);
